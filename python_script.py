1777efa105b7e526b8d56762cd60c28d2c4ba19e
Mike Evtushenko <mikestikka@gmail.com>
2022-01-09 15:23:18 +0300

    population layer added

--- python_script.py
+++ python_script.py
@@ -34,14 +34,15 @@ map_1 = folium.Map(location=[42.924869, -108.755439], zoom_start=5, tiles="Stame
 fg = folium.FeatureGroup(name="My Map")
 
 for latitude, longitude, elevation, name in zip(lat, lon, elev, name):
-
     iframe = folium.IFrame(html=html % (name, name, elevation), width=200, height=100)
     fg.add_child(
         folium.CircleMarker(location=[latitude, longitude], radius=5, popup=folium.Popup(iframe),
                             fill_color=assess_elevation(elevation), color=None, fill_opacity=0.7))
 
-
-fg.add_child(folium.GeoJson(data=(open("world.json", "r", encoding="utf-8-sig").read())))
+fg.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
+                            style_function=lambda x: {"fillColor": "green" if x["properties"]["POP2005"] < 10 ** 7 else
+                                                      "orange" if 10**7 <= x["properties"]["POP2005"] < 10**7 * 2
+                                                      else "red"}))
 
 map_1.add_child(fg)
 map_1.save("Map1.html")
