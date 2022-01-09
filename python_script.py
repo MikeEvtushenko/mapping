4f804004f0b72a924de944802bafaf6a3cffbcda
Mike Evtushenko <mikestikka@gmail.com>
2022-01-09 15:32:48 +0300

    layer controls added, final commit

--- python_script.py
+++ python_script.py
@@ -31,18 +31,20 @@ Height: %s m
 map_1 = folium.Map(location=[42.924869, -108.755439], zoom_start=5, tiles="Stamen Terrain")
 
 # fg = feature group - отдельный объект, содержащий слои, точки и т.д.
-fg = folium.FeatureGroup(name="My Map")
-
+fgv = folium.FeatureGroup(name="Volcanoes")
+fgp = folium.FeatureGroup(name="Population")
 for latitude, longitude, elevation, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, elevation), width=200, height=100)
-    fg.add_child(
+    fgv.add_child(
         folium.CircleMarker(location=[latitude, longitude], radius=5, popup=folium.Popup(iframe),
                             fill_color=assess_elevation(elevation), color=None, fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
-                            style_function=lambda x: {"fillColor": "green" if x["properties"]["POP2005"] < 10 ** 7 else
-                                                      "orange" if 10**7 <= x["properties"]["POP2005"] < 10**7 * 2
-                                                      else "red"}))
+fgp.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
+                             style_function=lambda x: {"fillColor": "green" if x["properties"]["POP2005"] < 10 ** 7 else
+                             "orange" if 10 ** 7 <= x["properties"]["POP2005"] < 10 ** 7 * 2
+                             else "red"}))
 
-map_1.add_child(fg)
+map_1.add_child(fgv)
+map_1.add_child(fgp)
+map_1.add_child(folium.LayerControl())
 map_1.save("Map1.html")
